bcc1f9dbd31e4ccefc37d8de2ba6f3b62c6bfb1a
stejlYura <stejlyura19@gmail.com>
2025-05-01 22:43:39 +0300

    Next commit

--- src/typescript_code/base.ts
+++ src/typescript_code/base.ts
@@ -18,3 +18,13 @@ class Car implements CarProject {
         this.price = price
     }
 }
+
+const BMW1 = new Car("BMW", 1, "gray" , 20000)
+const BMW2 = new Car("BMW", 2, "red", 23000)
+const BMW3 = new Car("BMW", 3, "black", 27000)
+
+const CarsMass = [
+    BMW1, BMW2, BMW3
+]
+
+console.table(CarsMass)
